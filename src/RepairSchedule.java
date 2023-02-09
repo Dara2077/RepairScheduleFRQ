6611273b751a7305257b2430e985f61e29064faa
dara2077 <dbenyacar1@gmail.com>
2023-02-09 09:08:20 -0500

    Finished

--- src/RepairSchedule.java
+++ src/RepairSchedule.java
@@ -28,6 +28,20 @@ public class RepairSchedule
     public boolean addRepair(int m, int b)
     {
         /* to be implemented in part (a) */
+        boolean avail = true;
+        for (CarRepair element: schedule){
+            if (element.getMechanicNum() == m || element.getBayNum() == b){
+                avail = false;
+            }
+        }
+        if (avail){
+            CarRepair newRepair = new CarRepair(m, b);
+            schedule.add(newRepair);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /** Returns an ArrayList containing the mechanic identifiers of all available mechanics,
@@ -36,6 +50,19 @@ public class RepairSchedule
     public ArrayList<Integer> availableMechanics()
     {
         /* to be implemented in part (b) */
+        ArrayList<Integer> availM = new ArrayList<Integer>();
+        for (int i = 0; i < 7; i ++){
+            boolean found = false;
+            for (CarRepair element: schedule){
+                if (element.getMechanicNum() == i){
+                    found = true;
+                }
+            }
+            if (!found){
+                availM.add(i);
+            }
+        }
+        return availM;
     }
 
     /** Removes an element from schedule when a repair is complete. */
